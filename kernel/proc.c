6e4ebc669f3e38d4d7728ba7b13192f44696aa1b
sjx <sjtu_xiaosong@sjtu.edu.cn>
2022-08-08 07:47:05 +0800

    pgtbl: finish the task of speeding system call

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,15 @@ found:
     return 0;
   }
 
+  // Allocate a speed up page.
+  #ifdef LAB_PGTBL
+  if((p->usyscall = (struct usyscall *)kalloc()) == 0) {
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  #endif
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -140,6 +149,10 @@ found:
   memset(&p->context, 0, sizeof(p->context));
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
+  
+  #ifdef LAB_PGTBL
+  p->usyscall->pid = p->pid;
+  #endif
 
   return p;
 }
@@ -164,6 +177,11 @@ freeproc(struct proc *p)
   p->killed = 0;
   p->xstate = 0;
   p->state = UNUSED;
+  #ifdef LAB_PGTBL
+  if(p->usyscall) 
+    kfree((void*)p->usyscall);
+  p->usyscall = 0;
+  #endif
 }
 
 // Create a user page table for a given process,
@@ -196,6 +214,14 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  #ifdef LAB_PGTBL
+  if(mappages(pagetable, USYSCALL, PGSIZE, 
+              (uint64)(p->usyscall), PTE_R | PTE_U) < 0) {
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+  #endif 
+
   return pagetable;
 }
 
@@ -206,6 +232,9 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  #ifdef LAB_PGTBL
+  uvmunmap(pagetable, USYSCALL, 1, 0);
+  #endif
   uvmfree(pagetable, sz);
 }
 
